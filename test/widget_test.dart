d5fdbfa2d10f7c28c0ad593f4a7ee8badf8e21ad
Andy Retinafunk <retinafunk@gmail.com>
2026-05-15 12:04:42 +0200

    Add unit tests for ProductRepositoryImpl and include mock dependencies

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,11 +8,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:product_catalog_offline_first_for_strategix_job/main.dart';
+import 'package:offline_products_catalog_app_github_copilot/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ProductsCatalogueApp());
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
